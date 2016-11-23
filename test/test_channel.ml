fae12076f6d0748f3742ea4ce9fac1f0162a2a63
Anil Madhavapeddy <anil@recoil.org>
2016-11-23 11:54:29 +0000

    Refactor CHANNEL types to be exception-less and use `result`
    - remove `read_stream` and `io_stream` as Lwt_stream is difficult to
      combine with error handling
    - remove `read_until` and only expose the line handling functions.
      I'm also considering moving the line-handling functions into a
      separate signature since most consumers of CHANNEL only want the
      buffering logic.
    - no more exceptions, just use `result` and propagate `FLOW.error`
      in return values.

--- test/test_channel.ml
+++ test/test_channel.ml
@@ -5,8 +5,10 @@ open Common
    handled properly *)
 module Channel = Channel.Make(Fflow)
 
-let err_read ch =
-  fail "character %c was returned from Channel.read_char on an empty flow" ch
+let err_read = function
+  | Ok (`Data ch) -> fail "character %c was returned from Channel.read_char on an empty flow" ch
+  | Ok `Eof -> Lwt.return ()
+  | Error (`Msg m) -> fail "unexpected error: %s" m
 
 let err_no_exception () = fail "no exception"
 let err_wrong_exception e = fail "wrong exception: %s" (Printexc.to_string e)
@@ -14,52 +16,25 @@ let err_wrong_exception e = fail "wrong exception: %s" (Printexc.to_string e)
 let test_read_char_eof () =
   let f = Fflow.make () in
   let c = Channel.create f in
-  let try_char_read () = Channel.read_char c >>= err_read in
-  Lwt.try_bind
-    (try_char_read)
-    err_no_exception (* "success" case (no exceptions) *)
-    (function
-      | End_of_file -> Lwt.return_unit
-      | e -> err_wrong_exception e)
-
-let test_read_until_eof () =
-  let input =
-    Fflow.input_string "I am the very model of a modern major general"
-  in
-  let f = Fflow.make ~input () in
-  let c = Channel.create f in
-  Channel.read_until c 'v' >>= function
-  | true, buf ->
-    assert_cstruct "wrong flow prefix"
-      (Cstruct.of_string "I am the ") buf;
-    Channel.read_until c '\xff' >>= fun (found, buf) ->
-    assert_bool "found a char that couldn't have been there in read_until"
-      false found;
-    assert_cstruct "wrong flow suffix"
-      (Cstruct.of_string "ery model of a modern major general") buf;
-    Channel.read_until c '\n' >>= fun (found, buf) ->
-    assert_bool "found a char after EOF in read_until"
-      false found;
-    assert_int "wrong flow size" 0 (Cstruct.len buf);
-    Lwt.return_unit
-  | false, _ ->
-    OUnit.assert_failure "thought we couldn't find a 'v' in input test"
+  Channel.read_char c >>= err_read
 
 let test_read_line () =
   let input = "I am the very model of a modern major general" in
   let f = Fflow.make ~input:(Fflow.input_string input) () in
   let c = Channel.create f in
-  Channel.read_line c  >>= fun buf ->
-  assert_string "read line" input (Cstruct.copyv buf);
-  Lwt.return_unit
+  Channel.read_line c >|= function
+  | Ok (`Data buf) -> assert_string "read line" input (Cstruct.copyv buf)
+  | Ok `Eof -> fail "eof"
+  | Error (`Msg m) -> fail "error: %s" m
 
 let test_read_exactly () =
   let input = "I am the very model of a modern major general" in
   let f = Fflow.make ~input:(Fflow.input_string input) () in
   let c = Channel.create f in
-  Channel.read_exactly ~len:4 c >>= fun bufs ->
-  assert_int "wrong length" 4 (Cstruct.(len (concat bufs)));
-  Lwt.return_unit
+  Channel.read_exactly ~len:4 c >|= function
+  | Ok (`Data bufs) -> assert_int "wrong length" 4 (Cstruct.(len (concat bufs)))
+  | Ok `Eof -> fail "eof"
+  | Error (`Msg m) -> fail "error: %s" m
 
 let test_read_until_eof_then_write () =
   let str = "I am the very model of a modern major general" in
@@ -77,12 +52,13 @@ let test_read_until_eof_then_write () =
   (* Should read to EOF: *)
   Channel.read_line c >>= fun _ ->
   Channel.write_line c "Even though I've read to EOF, I should be able to write";
-  Channel.flush c >>= fun () ->
-  Lwt.return_unit
+  Channel.flush c >|= function
+  | Ok () -> ()
+  | Error `Closed -> fail "error: closed"
+  | Error (`Msg m) -> fail "error: %s" m
 
 let suite = [
   "read_char + EOF" , `Quick, test_read_char_eof;
-  "read_until + EOF", `Quick, test_read_until_eof;
   "read_line"       , `Quick, test_read_line;
   "read_exactly"    , `Quick, test_read_exactly;
   "write after read EOF", `Quick, test_read_until_eof_then_write;
